d38d7701c889eb12a5e025c24f9f8372ec8016bd
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-01-24 20:22:56 +0900

    no message

--- adwh_bodyparts/adwh_bodyparts.ino
+++ adwh_bodyparts/adwh_bodyparts.ino
@@ -1,6 +1,6 @@
 #include <arduwatch.h>
 
-
+short hits=1;
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3); // RX, TX
 Hero park;
@@ -31,9 +31,11 @@ void loop() // run over and over
       Serial.print(atk);
 
       Serial.print("         ");
-      Serial.println("doscount");
+       Serial.print(hits);
+        Serial.print("         ");
+      Serial.println("discount");
       Serial.println(park.Hero_print_HP_Now());
-
+    hits++;
 
       atk = 0;
       delay(1000);
@@ -43,9 +45,19 @@ void loop() // run over and over
   }
   if (park.Hero_print_HP_Now() <= 0) {
     digitalWrite(7, HIGH);
+    Serial.println("You Died");
+    atk=0;
+    hits=1;
+     for(int i;i<=20;i++){
+      Serial.print(':');
+      delay(100);
+     }
+    park.Hero_Revive();
+    Serial.println("You are revived");
 
   } else {
     digitalWrite(7, LOW);
+  atk=0;
   }
   atk = 0;
 }
